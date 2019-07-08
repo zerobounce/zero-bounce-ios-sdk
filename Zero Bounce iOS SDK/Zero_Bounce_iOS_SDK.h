c04f741ec9c9e0f5382aca94cbf1d0874d2b4d63
ZeroBounce <integrations@zerobounce.net>
2019-07-08 18:37:59 +0300

    Initial commit

---
+++ Zero Bounce iOS SDK/Zero_Bounce_iOS_SDK.h
@@ -0,0 +1,19 @@
+//
+//  Zero_Bounce_iOS_SDK.h
+//  Zero Bounce iOS SDK
+//
+//  Created by Mount Software on 08/07/2019.
+//  Copyright © 2019 Mount Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Zero_Bounce_iOS_SDK.
+FOUNDATION_EXPORT double Zero_Bounce_iOS_SDKVersionNumber;
+
+//! Project version string for Zero_Bounce_iOS_SDK.
+FOUNDATION_EXPORT const unsigned char Zero_Bounce_iOS_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Zero_Bounce_iOS_SDK/PublicHeader.h>
+
+
---
+++ Zero Bounce iOS SDKTests/Zero_Bounce_iOS_SDKTests.swift
@@ -0,0 +1,34 @@
+//
+//  Zero_Bounce_iOS_SDKTests.swift
+//  Zero Bounce iOS SDKTests
+//
+//  Created by Mount Software on 08/07/2019.
+//  Copyright © 2019 Mount Software. All rights reserved.
+//
+
+import XCTest
+@testable import Zero_Bounce_iOS_SDK
+
+class Zero_Bounce_iOS_SDKTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
